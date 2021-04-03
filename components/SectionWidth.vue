614513a8a65fd073f2d1cc5b782c696de9d43e42
JavierGonzalez <javier.e.glz@gmail.com>
2021-04-03 09:55:13 -0500

    add a layout component for section witdh

---
+++ components/SectionWidth.vue
@@ -0,0 +1,22 @@
+<template>
+  <component
+    :is="element"
+    class="md:px-12 lg:px-24 xl:px-48 max-w-screen-2xl mx-auto"
+    :class="[classList]"
+  >
+      <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: "SectionWidth",
+  props: {
+	element: {
+      type: String,
+	  default: "section"
+	},
+  }
+};
+</script>
+
